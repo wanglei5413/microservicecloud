7a9d7e6b085d83454c895e1d1048f0504e5d52a9
wl <wl@613.com>
2019-12-23 23:29:43 +0800

    部门微服务注册到euruka

--- microservicecloud-provider-dept-8001/src/main/java/com/techlei/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/techlei/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.techlei.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient//服务端启动后注册到eureka注册中心
 public class DeptProvider8001_App {
 	
 	public static void main(String[] args) {
