ff404335f11b1e84678050d5a2144a5d40a17008
wl <wl@613.com>
2019-12-27 00:55:10 +0800

    配置默认负载均衡算法

--- microservicecloud-consumer-dept-80/src/main/java/com/techlei/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/techlei/springcloud/cfgbeans/ConfigBean.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.BestAvailableRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.WeightedResponseTimeRule;
+import com.netflix.loadbalancer.ZoneAvoidanceRule;
+
 @Configuration
 public class ConfigBean {
 
@@ -12,5 +20,16 @@ public class ConfigBean {
 	@LoadBalanced  //Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public IRule MyRule() {
+//		return new RoundRobinRule();//轮询 默认
+//		return new RandomRule();//随机 
+//		return new RetryRule();//retry 
+//		return new AvailabilityFilteringRule();//不可用性过滤 
+//		return new BestAvailableRule();//最佳可用 
+//		return new WeightedResponseTimeRule();//权重相应时间
+		return new ZoneAvoidanceRule();//
 	}
 }
